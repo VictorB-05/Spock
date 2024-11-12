9a49509b5e3dfb5b0a194a8c47a4eaf51084a925
Victor <victorbejar2005@gmail.com>
2024-11-12 13:56:58 +0100

    se ha implementado la logica de ganar se ha y perder y se le dice a los usuarios atraves de dos labels

--- Interfaz.py
+++ Interfaz.py
@@ -1,9 +1,24 @@
+import random
 import tkinter
-from cProfile import label
 from tkinter import *
-
-import logica
 import simbolo
+
+
+def juego(opcion):
+    numero_aleatorio = random.randint(1, 5)
+    cpu = simbolo.Simbolo(numero_aleatorio)
+    print(cpu.name)
+    rel = opcion.comparar(cpu)
+    if rel == 1:
+        resultado.set("Has ganado")
+        jugada.set(f"{opcion.name} ganan a {cpu.name}")
+    elif rel == -1:
+        resultado.set("Has perdido")
+        jugada.set(f"{cpu.name} ganan a {opcion.name}")
+    else:
+        resultado.set("Empate")
+        jugada.set(f"{opcion.name} y {cpu.name} es ")
+
 
 raiz = Tk()
 raiz.resizable(width=False, height=False)
@@ -13,23 +28,28 @@ raiz.geometry('1000x600')
 frame1 = Frame(raiz,width=1000,height=600,bg="#ffb0c8",borderwidth=10,relief="ridge")
 frame1.place(x=0,y=0)
 
-Piedra = Button(frame1, width=12, bg="blue", text="Piedra", command=lambda : logica.juego(simbolo.Simbolo.Piedra))
+#botones para jugar
+Piedra = Button(frame1, width=12, bg="blue", text="Piedra", command=lambda : juego(simbolo.Simbolo.Piedra))
 Piedra.place(x=15, y= 400)
-Papel = Button(frame1, width=12,bg="blue",text="Papel" ,command=lambda : logica.juego(simbolo.Simbolo.Papel))
+Papel = Button(frame1, width=12,bg="blue",text="Papel" ,command=lambda : juego(simbolo.Simbolo.Papel))
 Papel.place(x=214, y= 400)
-Tijeras = Button(frame1, width=12,bg="blue",text="Tijeras" ,command=lambda : logica.juego(simbolo.Simbolo.Tijeras))
+Tijeras = Button(frame1, width=12,bg="blue",text="Tijeras" ,command=lambda : juego(simbolo.Simbolo.Tijeras))
 Tijeras.place(x=432, y= 400)
-Lagarto = Button(frame1, width=12,bg="blue",text="Lagarto" ,command=lambda : logica.juego(simbolo.Simbolo.Lagarto))
+Lagarto = Button(frame1, width=12,bg="blue",text="Lagarto" ,command=lambda : juego(simbolo.Simbolo.Lagarto))
 Lagarto.place(x=650, y= 400)
-Spok = Button(frame1, width=12,bg="blue",text="Spok" ,command=lambda : logica.juego(simbolo.Simbolo.Spok))
-Spok.place(x=858, y= 400)
+Spock = Button(frame1, width=12, bg="blue", text="Spok", command=lambda: juego(simbolo.Simbolo.Spock))
+Spock.place(x=858, y= 400)
+#labels para dar resultados
 resultado = tkinter.StringVar()
+resultado.set("inicio")
 label1 = Label(frame1,textvariable=resultado, font=("Arial",30))
-label1.place(x=0,y=0)
-# Esperamos a que la ventana se renderice para medir el ancho
-raiz.update()  # Se asegura que los widgets se hayan renderizado
 
-# Imprimimos el tamaño del botón en píxeles
-print(Piedra.winfo_width())
+jugada = tkinter.StringVar()
+jugada.set("-------------")
+ljugada = Label(frame1,textvariable=jugada, font=("Arial",20))
+
+label1.place(x=300,y=200,width=400)
+ljugada.place(x=300,y=170,width=400)
 
 raiz.mainloop()
+
--- simbolo.py
+++ simbolo.py
@@ -4,38 +4,40 @@ class Simbolo(Enum):
     Piedra = 1
     Papel = 2
     Tijeras = 3
-    Lagarto = 1
-    Spok = 2
+    Lagarto = 4
+    Spock = 5
 
 
-def comparar(self, cpu):
-    if self == cpu:
-        return 0
-    else:
-        match self:
-            case 1:
-                if cpu == 3:
-                    return 1
-                elif cpu == 4:
-                    return 1
-            case 2:
-                if cpu == 1:
-                    return 1
-                elif cpu == 5:
-                    return 1
-            case 3:
-                if cpu == 2:
-                    return 1
-                elif cpu == 4:
-                    return 1
-            case 4:
-                if cpu == 2:
-                    return 1
-                elif cpu == 5:
-                    return 1
-            case 5:
-                if cpu == 3:
-                    return 1
-                elif cpu == 1:
-                    return 1
-    return -1
+    def comparar(self, cpu):
+        if self == cpu:
+            return 0
+        else:
+            match self.value:
+                case 1:
+                    if cpu.value == 3:
+                        return 1
+                    elif cpu.value == 4:
+                        return 1
+                case 2:
+                    if cpu.value == 1:
+                        return 1
+                    elif cpu.value == 5:
+                        return 1
+                case 3:
+                    if cpu.value == 2:
+                        return 1
+                    elif cpu.value == 4:
+                        return 1
+                case 4:
+                    if cpu.value == 2:
+                        return 1
+                    elif cpu.value == 5:
+                        return 1
+                case 5:
+                    if cpu.value == 3:
+                        return 1
+                    elif cpu.value == 1:
+                        return 1
+                case _:
+                    print("hey")
+        return -1
